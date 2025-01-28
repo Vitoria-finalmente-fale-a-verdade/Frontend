2cc13e5e3e8ae40df2dec368c1c0e9dfe6dd9e95
pedro-h-o-soares <pedrosoares77744@gmail.com>
2025-01-27 23:41:49 -0300

    feat: red color theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { MyTheme } from './theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,8 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura
+            preset: MyTheme
         }
-        })
+    })
   ]
 };
