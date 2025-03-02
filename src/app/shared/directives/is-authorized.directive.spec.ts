d17c7ed30f0c812da8d5f8ff57c3489ab71db498
pedro-h-o-soares <pedrosoares77744@gmail.com>
2025-03-01 23:45:30 -0300

    tests: fix is-authorized directive tests

--- src/app/shared/directives/is-authorized.directive.spec.ts
+++ src/app/shared/directives/is-authorized.directive.spec.ts
@@ -1,8 +1,10 @@
 import { IsAuthorizedDirective } from './is-authorized.directive';
+import {AuthService} from '../../services/auth.service';
+import {inject, TemplateRef, ViewContainerRef} from '@angular/core';
 
 describe('IsAuthorizedDirective', () => {
   it('should create an instance', () => {
-    const directive = new IsAuthorizedDirective();
+    const directive = new IsAuthorizedDirective(inject(AuthService), inject(TemplateRef), inject(ViewContainerRef));
     expect(directive).toBeTruthy();
   });
 });
