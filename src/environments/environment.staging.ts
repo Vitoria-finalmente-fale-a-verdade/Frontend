15c37d0671a88a362bb7a1314bd6f2639ba6602b
Pedro Henrique De Oliveira Soares <75043682+pedro-h-o-soares@users.noreply.github.com>
2025-04-04 08:27:33 -0300

    Create environment.staging.ts

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://smart-farm-api-1041106814128.us-east1.run.app',
+  ibgeDataUrl: 'https://servicodados.ibge.gov.br/api/v1/',
+};
