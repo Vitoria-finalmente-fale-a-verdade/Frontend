7efd7fb4220a142b859d9df06bbde66570f7b4de
pedro-h-o-soares <pedrosoares77744@gmail.com>
2025-04-04 21:40:14 -0300

    feat: json-date interceptor

---
+++ src/app/shared/interceptors/json-date.interceptor.ts
@@ -0,0 +1,41 @@
+import {HttpEvent, HttpInterceptorFn, HttpResponse} from '@angular/common/http';
+import {map} from 'rxjs';
+
+export const jsonDateInterceptor: HttpInterceptorFn = (req, next) => {
+  const _isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?Z?$/
+
+  const isIsoDateString = (value: any): boolean => {
+    if (value === null || value === undefined) {
+      return false;
+    }
+    if (typeof value === 'string'){
+      return _isoDateFormat.test(value);
+    }    return false;
+  }
+
+  const convert = (body: any) => {
+    if (body === null || body === undefined) {
+      return body;
+    }
+    if (typeof body !== 'object') {
+      return body;
+    }
+    for (const key of Object.keys(body)) {
+      const value = body[key];
+      if (isIsoDateString(value)) {
+        body[key] = new Date(value);
+      } else if (typeof value === 'object') {
+        convert(value);
+      }
+    }
+  }
+
+  return next(req).pipe(map( (val: HttpEvent<any>) => {
+    if (val instanceof HttpResponse){
+      const body = val.body;
+      convert(body);
+    }
+    console.log(val);
+    return val;
+  }));
+};
