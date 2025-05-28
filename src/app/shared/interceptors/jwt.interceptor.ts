5321b72127f06acd29db03e37f6c5ca8b8c951e2
pedro-h-o-soares <pedrosoares77744@gmail.com>
2025-05-27 21:00:41 -0300

    ref: remove token reference

--- src/app/shared/interceptors/jwt.interceptor.ts
+++ src/app/shared/interceptors/jwt.interceptor.ts
@@ -5,7 +5,6 @@ import {AuthService} from '../../services/auth.service';
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
 
-  const token = authService.token;
   const customer = authService.customer;
   const property = authService.property;
 
